7eab89863fc55ed8382513ace5b989926af409ab
Dasha <kalabanova_dasha@mail.ru>
2023-10-18 22:18:18 +0300

    Add dockstrings

--- bio_files_processor.py
+++ bio_files_processor.py
@@ -1,4 +1,14 @@
-def convert_multiline_fasta_to_oneline(input_fasta,output_fasta):
+def convert_multiline_fasta_to_oneline(input_fasta: str, output_fasta: str):
+    """
+        A function that makes one of several fasta sequences
+
+        Arguments:
+        - input_fasta (str): the path to fasta file
+        - output_fasta (str): the path to the new output fasta file
+
+        Return:
+        - File with combined sequences
+        """
     with open(input_fasta) as fasta_file, open(output_fasta + '.fasta', 'w') as output_file:
         lines_new = []
         for line in fasta_file:
@@ -10,11 +20,21 @@ def convert_multiline_fasta_to_oneline(input_fasta,output_fasta):
         print(lines_new)
         return output_file.write("\n".join(lines_new))
 
-#C:\Users\Ярослав\Downloads\example_multiline_fasta.fasta
 
-def select_genes_from_gbk_to_fasta (input_gbk, genes, output_fasta: object, n_before = 1, n_after = 1):
+def select_genes_from_gbk_to_fasta (input_gbk: str, genes: list[str], output_fasta: str, n_before: int = 1,
+                                    n_after: int = 1):
     """
-    :type output_fasta: object
+    A function that searches for neighboring genes with a user-defined gene
+
+        Arguments:
+        - input_gbk (str): the path to gbk file
+        - output_fasta (str): the path to the new output fasta file
+        - genes (list(str)): list with users genes
+        - n_before (int): number of genes up to
+        - n_after (int): number of genes after
+
+        Return:
+        - A file with the genes located next to the user's genes
     """
     with open(input_gbk) as gbk_file, open (output_fasta + '.fasta', 'w') as fasta_file:
         lines = gbk_file.readlines()
@@ -41,14 +61,3 @@ def select_genes_from_gbk_to_fasta (input_gbk, genes, output_fasta: object, n_be
                         list_res.append(''.join(lines[trans_list[gene_list.index(genline)]-n_before+1:genline-n_before]))
                     for i in range(n_after):
                         list_res.append(''.join(lines[trans_list[gene_list.index(genline)+n_after+1]:gene_list[gene_list.index(genline)+n_after+1]-1]))
-
-
-
-
-
-
-
-
-
-select_genes_from_gbk_to_fasta("C:\\Users\Ярослав\Downloads\example_gbk.gbk", genes=["pxpB"], output_fasta= 'sdfa')
-# "C:\Users\Ярослав\Downloads\example_gbk.gbk"
