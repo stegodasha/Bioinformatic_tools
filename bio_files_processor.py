758e1952dbc8792cd433f974f21ad8cfae359dec
Dasha <kalabanova_dasha@mail.ru>
2023-10-18 22:39:06 +0300

    Correct linters

--- bio_files_processor.py
+++ bio_files_processor.py
@@ -21,8 +21,8 @@ def convert_multiline_fasta_to_oneline(input_fasta: str, output_fasta: str):
         return output_file.write("\n".join(lines_new))
 
 
-def select_genes_from_gbk_to_fasta (input_gbk: str, genes: list[str], output_fasta: str, n_before: int = 1,
-                                    n_after: int = 1):
+def select_genes_from_gbk_to_fasta(input_gbk: str, genes: list[str], output_fasta: str, n_before: int = 1,
+                                   n_after: int = 1):
     """
     A function that searches for neighboring genes with a user-defined gene
 
@@ -36,16 +36,16 @@ def select_genes_from_gbk_to_fasta (input_gbk: str, genes: list[str], output_fas
         Return:
         - A file with the genes located next to the user's genes
     """
-    with open(input_gbk) as gbk_file, open (output_fasta + '.fasta', 'w') as fasta_file:
+    with open(input_gbk) as gbk_file, open(output_fasta + '.fasta', 'w') as fasta_file:
         lines = gbk_file.readlines()
         for line in range(len(lines)):
-            lines[line] = lines[line][0:len(lines[line])-2]
+            lines[line] = lines[line][0:len(lines[line]) - 2]
 
         gene_list = []
         trans_list = []
         transgene_list = []
         for line in range(len(lines)):
-            if lines[line].find('/gene=') != -1 :
+            if lines[line].find('/gene=') != -1:
                 gene_list.append(line)
                 transgene_list.append(line)
             if lines[line].find('/translation=') != -1:
@@ -58,6 +58,9 @@ def select_genes_from_gbk_to_fasta (input_gbk: str, genes: list[str], output_fas
             for genline in gene_list:
                 if lines[genline].find(word) != -1:
                     for i in range(n_before):
-                        list_res.append(''.join(lines[trans_list[gene_list.index(genline)]-n_before+1:genline-n_before]))
+                        list_res.append(
+                            ''.join(lines[trans_list[gene_list.index(genline)] - n_before + 1:genline - n_before]))
                     for i in range(n_after):
-                        list_res.append(''.join(lines[trans_list[gene_list.index(genline)+n_after+1]:gene_list[gene_list.index(genline)+n_after+1]-1]))
+                        list_res.append(''.join(lines[trans_list[gene_list.index(genline) + n_after + 1]:gene_list[
+                                                                                                             gene_list.index(
+                                                                                                                 genline) + n_after + 1] - 1]))
